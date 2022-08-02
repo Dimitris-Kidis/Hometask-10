07160be1575124449a971a7e4eb9e2810dfb07ff
Dmitrii Romanenco <dmitrii.romanenco@amdaris.com>
2022-08-02 14:51:13 +0300

    Minor Changes & Cleaning Up The Code

--- Hometask 10/Program.cs
+++ Hometask 10/Program.cs
@@ -27,17 +27,17 @@ namespace App
             // Colletion
             var list = new List<Person>
             {
-                new Person(0, "John", "Smith", 22, "Baker St. 221b", "London", "01-01-2000", "+2 453 544 553", "M"),
-                new Person(1, "Emma", "Doe", 14, "Lupu St. 52", "Moscow", "05-07-1997", "+2 453 544 553", "F"),
-                new Person(2, "Liam", "Jones", 43, "Bucuresti St. 84", "Kiev", "14-03-1988", "+2 453 544 553", "M"),
-                new Person(3, "Amelia", "Williams", 17, "Armeneasca St. 93", "Chisinau", "07-12-2009", "+2 453 544 553", "F"),
-                new Person(4, "Oliver", "Brown", 12, "Creanga St. 11", "Havana", "11-04-2008", "+2 453 544 553", "M"),
-                new Person(5, "Mia", "Johnson", 32, "31 August St. 32", "Cairo", "25-06-2004", "+2 453 544 553", "F"),
-                new Person(6, "James", "Taylor", 19, "Columna St. 6", "Beijing", "09-01-1998", "+2 453 544 553", "M"),
-                new Person(7, "Sophia", "Evans", 21, "Siusev St. 97", "Tallinn", "29-07-1995", "+2 453 544 553", "F"),
-                new Person(8, "William", "Wilson", 13, "Puskin St. 53", "Tbilisi", "12-09-2001", "+2 453 544 553", "M"),
-                new Person(9, "Bella", "Roberts", 46, "Matievici St. 32", "Paris", "19-04-2002", "+2 453 544 553", "F"),
-                new Person(10, "Benjamin", "Thomas", 65, "Izvor St. 2", "Rome", "02-11-1978", "+2 453 544 553", "M")
+                new Person(0, "John", "Smith", "Baker St. 221b", "London", "01-01-2000", "+2 453 544 553", "M"),
+                new Person(1, "Emma", "Doe", "Lupu St. 52", "Moscow", "05-07-1997", "+2 453 544 553", "F"),
+                new Person(2, "Liam", "Jones", "Bucuresti St. 84", "Kiev", "14-03-1988", "+2 453 544 553", "M"),
+                new Person(3, "Amelia", "Williams", "Armeneasca St. 93", "Chisinau", "07-12-2009", "+2 453 544 553", "F"),
+                new Person(4, "Oliver", "Brown", "Creanga St. 11", "Havana", "11-04-2008", "+2 453 544 553", "M"),
+                new Person(5, "Mia", "Johnson", "31 August St. 32", "Cairo", "25-06-2004", "+2 453 544 553", "F"),
+                new Person(6, "James", "Taylor", "Columna St. 6", "Beijing", "09-01-1998", "+2 453 544 553", "M"),
+                new Person(7, "Sophia", "Evans", "Siusev St. 97", "Tallinn", "29-07-1995", "+2 453 544 553", "F"),
+                new Person(8, "William", "Wilson", "Puskin St. 53", "Tbilisi", "12-09-2001", "+2 453 544 553", "M"),
+                new Person(9, "Bella", "Roberts", "Matievici St. 32", "Paris", "19-04-2002", "+2 453 544 553", "F"),
+                new Person(10, "Benjamin", "Thomas", "Izvor St. 2", "Rome", "02-11-1978", "+2 453 544 553", "M")
             };
 
 
@@ -63,17 +63,18 @@ namespace App
                 Console.WriteLine();
                 Console.ForegroundColor = ConsoleColor.White;
             };
-
+            
 
             // One more usage of an anonymous delegate
-            PrintPeopleOlderThanEightteen(list);
+            //PrintPeopleOlderThanEightteen(list);
 
             List<Person> PeopleOlderThan18 = list.FindAll(delegate (Person element)
             {
                 //if (DateTime.Now.Year - Int32.Parse(item.BirthDate.Substring(6, 4)) > 18) return
                     return DateTime.Now.Year - Int32.Parse(element.BirthDate.Substring(6, 4)) > 18;
             });
-            Console.WriteLine("----------------------------");
+            Console.WriteLine("------------ People Over 18 -------------");
+            
             foreach (Person element in PeopleOlderThan18)
             {
                 Console.WriteLine(element.ToString());
@@ -84,7 +85,7 @@ namespace App
             CustomNameOperations op5 = (List<Person> list) =>
             {
                 Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("----- Names Beginning With J -----");
+                Console.WriteLine("\n----- Names Beginning With J -----");
                 foreach (var item in list)
                 {
                     if (item.FirstName.Substring(0,1) == "J") Console.WriteLine(item.FirstName + " " + item.LastName);
@@ -103,26 +104,30 @@ namespace App
                     Console.WriteLine(item.ToString());
                 }
             };
+            Console.WriteLine("------- List With Incremented IDs -------");
             op6(list);
 
 
             // Extenstion Method
-
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\n------- Check if a person with ID = 0 older than 24 -------");
             var olderThan24 = list.IsOlderThan(0, 24);
             Console.WriteLine(olderThan24);
+            Console.ForegroundColor = ConsoleColor.Green;
 
-
+            Console.WriteLine("\n------- Print people from London -------");
             var city = list.CheckCity("London"); 
             foreach (var item in city)
             {
                 Console.WriteLine(item.ToString());
             }
+            Console.ForegroundColor = ConsoleColor.White;
 
 
 
 
             // Select & Where
-
+            Console.WriteLine("\n------- Print people with ID greater than 5 and older than 20 -------");
             // Fluent Style
             var filteredListFluentStyle1 = list
                                 .Where(x => x.ID > 5 && x.Age > 20)
@@ -133,7 +138,9 @@ namespace App
             {
                 Console.WriteLine(item);
             }
+            Console.ForegroundColor = ConsoleColor.Yellow;
 
+            Console.WriteLine("\n------- Print people with ID less than 5 and under 20 -------");
             // Query Style
             var filteredListQueryStyle1 = from person in list
                                           where person.ID < 5 && person.Age < 20
@@ -160,6 +167,8 @@ namespace App
             {
                 Console.WriteLine(item.FirstName + " " + item.LastName);
             }
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.White;
         }
 
         public static void PrintAllMaleNames(List<Person> list)
@@ -202,13 +211,18 @@ namespace App
         public int ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public int Age { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
         public string BirthDate { get; }
         public string PhoneNumber { get; set; }
         public string Gender { get; set; }
-        public Person(int iD, string firstName, string lastName, int age, string address, string city, string birthDate, string phoneNumber, string gender)
+        public int Age {
+            get 
+            {
+                return DateTime.Now.Year - Int32.Parse(BirthDate.Substring(6, 4));
+            }
+        }
+        public Person(int iD, string firstName, string lastName, string address, string city, string birthDate, string phoneNumber, string gender)
         {
             ID = iD;
             FirstName = firstName;
@@ -218,7 +232,6 @@ namespace App
             BirthDate = birthDate;
             PhoneNumber = phoneNumber;
             Gender = gender;
-            Age = age;
         }
 
         public override string ToString()
@@ -228,5 +241,5 @@ namespace App
     }
 
 
-    
+   
 }
