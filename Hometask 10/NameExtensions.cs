f33cd8b1dc7f3d279d1b46fe3c8f5ccce85a2e42
Dmitrii Romanenco <dmitrii.romanenco@amdaris.com>
2022-08-02 11:03:08 +0300

    Hometask Finished

---
+++ Hometask 10/NameExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    public static class NameExtension
+    {
+        public static bool IsOlderThan(this List<Person> list, int ID, int age)
+        {
+            return DateTime.Now.Year - Int32.Parse(list[ID].BirthDate.Substring(6, 4)) > age;
+        }
+    }
+
+
+    public static class CityExtension
+    {
+        public static List<Person> CheckCity(this List<Person> list, string city)
+        {
+            List<Person> tmp = new List<Person>();
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i].City == city)
+                {
+                    tmp.Add(list[i]);
+                }
+            }
+            return tmp;
+        }
+    }
+}
--- Hometask 10/Program.cs
+++ Hometask 10/Program.cs
@@ -1,13 +1,16 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Collections;
+using System.Reflection;
 
 
 // 1. Create a collection ✅
 // 2. Use delegates ✅
 // 3. Rewrite using anonymous functions ✅
 // 4. Rewrite using lambda expressions ✅
-// 5. Using extension methods on the collection
-// 6. Using Select/Where operators on the collection
+// 5. Using extension methods on the collection ✅
+// 6. Using Select/Where operators on the collection ✅
 
 
 namespace App
@@ -24,17 +27,17 @@ namespace App
             // Colletion
             var list = new List<Person>
             {
-                new Person(0, "John", "Smith", "Baker St. 221b", "London", "01-01-2000", "+2 453 544 553", "M"),
-                new Person(1, "Emma", "Doe", "Lupu St. 52", "Moscow", "05-07-1997", "+2 453 544 553", "F"),
-                new Person(2, "Liam", "Jones", "Bucuresti St. 84", "Kiev", "14-03-1988", "+2 453 544 553", "M"),
-                new Person(3, "Amelia", "Williams", "Armeneasca St. 93", "Chisinau", "07-12-2009", "+2 453 544 553", "F"),
-                new Person(4, "Oliver", "Brown", "Creanga St. 11", "Havana", "11-04-2008", "+2 453 544 553", "M"),
-                new Person(5, "Mia", "Johnson", "31 August St. 32", "Cairo", "25-06-2004", "+2 453 544 553", "F"),
-                new Person(6, "James", "Taylor", "Columna St. 6", "Beijing", "09-01-1998", "+2 453 544 553", "M"),
-                new Person(7, "Sophia", "Evans", "Siusev St. 97", "Tallinn", "29-07-1995", "+2 453 544 553", "F"),
-                new Person(8, "William", "Wilson", "Puskin St. 53", "Tbilisi", "12-09-2001", "+2 453 544 553", "M"),
-                new Person(9, "Bella", "Roberts", "Matievici St. 32", "Paris", "19-04-2002", "+2 453 544 553", "F"),
-                new Person(10, "Benjamin", "Thomas", "Izvor St. 2", "Rome", "02-11-1978", "+2 453 544 553", "M")
+                new Person(0, "John", "Smith", 22, "Baker St. 221b", "London", "01-01-2000", "+2 453 544 553", "M"),
+                new Person(1, "Emma", "Doe", 14, "Lupu St. 52", "Moscow", "05-07-1997", "+2 453 544 553", "F"),
+                new Person(2, "Liam", "Jones", 43, "Bucuresti St. 84", "Kiev", "14-03-1988", "+2 453 544 553", "M"),
+                new Person(3, "Amelia", "Williams", 17, "Armeneasca St. 93", "Chisinau", "07-12-2009", "+2 453 544 553", "F"),
+                new Person(4, "Oliver", "Brown", 12, "Creanga St. 11", "Havana", "11-04-2008", "+2 453 544 553", "M"),
+                new Person(5, "Mia", "Johnson", 32, "31 August St. 32", "Cairo", "25-06-2004", "+2 453 544 553", "F"),
+                new Person(6, "James", "Taylor", 19, "Columna St. 6", "Beijing", "09-01-1998", "+2 453 544 553", "M"),
+                new Person(7, "Sophia", "Evans", 21, "Siusev St. 97", "Tallinn", "29-07-1995", "+2 453 544 553", "F"),
+                new Person(8, "William", "Wilson", 13, "Puskin St. 53", "Tbilisi", "12-09-2001", "+2 453 544 553", "M"),
+                new Person(9, "Bella", "Roberts", 46, "Matievici St. 32", "Paris", "19-04-2002", "+2 453 544 553", "F"),
+                new Person(10, "Benjamin", "Thomas", 65, "Izvor St. 2", "Rome", "02-11-1978", "+2 453 544 553", "M")
             };
 
 
@@ -103,6 +106,52 @@ namespace App
             op6(list);
 
 
+            // Extenstion Method
+
+            var olderThan24 = list.IsOlderThan(0, 24);
+            Console.WriteLine(olderThan24);
+
+
+            var city = list.CheckCity("London"); 
+            foreach (var item in city)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+
+
+
+            // Select & Where
+
+            // Fluent Style
+            var filteredListFluentStyle1 = list
+                                .Where(x => x.ID > 5 && x.Age > 20)
+                                .OrderBy(x => x.Age)
+                                .Select(x => x.ID + " " + x.FirstName + " " + x.LastName + ", " + x.Age);
+            
+            foreach (var item in filteredListFluentStyle1)
+            {
+                Console.WriteLine(item);
+            }
+
+            // Query Style
+            var filteredListQueryStyle1 = from person in list
+                                          where person.ID < 5 && person.Age < 20
+                                          orderby person.Age
+                                          select person.ID + " " + person.FirstName + " " + person.LastName + ", " + person.Age;
+
+
+            foreach (var item in filteredListQueryStyle1)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+
+
+
+
         }
 
         public static void PrintNames(List<Person> list) {
@@ -148,17 +197,18 @@ namespace App
     }
 
 
-    class Person
+    public class Person
     {
         public int ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public int Age { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
         public string BirthDate { get; }
         public string PhoneNumber { get; set; }
         public string Gender { get; set; }
-        public Person(int iD, string firstName, string lastName, string address, string city, string birthDate, string phoneNumber, string gender)
+        public Person(int iD, string firstName, string lastName, int age, string address, string city, string birthDate, string phoneNumber, string gender)
         {
             ID = iD;
             FirstName = firstName;
@@ -168,6 +218,7 @@ namespace App
             BirthDate = birthDate;
             PhoneNumber = phoneNumber;
             Gender = gender;
+            Age = age;
         }
 
         public override string ToString()
@@ -175,4 +226,7 @@ namespace App
             return $"[{ID,2}]: {FirstName,10} {LastName,10}, {BirthDate,10}, from {Address}, {City}. ({PhoneNumber})";
         }
     }
+
+
+    
 }
